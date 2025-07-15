673078b3d2835169f34488b76e831caa6b2d7571
yeongbee <handfaste@naver.com>
2025-07-15 20:21:05 +0900

    feat: API 문서 추가 및 코드 리팩토링
    api.txt 파일을 추가하여 API 명세를 문서화하고, 사용자 관련 DTO를 리팩토링했습니다.

--- src/main/java/com/reservation/user/users/controller/UserController.java
+++ src/main/java/com/reservation/user/users/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.reservation.user.users.controller;
 
 import com.reservation.user.users.dto.UserDto;
+import com.reservation.user.users.dto.UserEntityDto;
+import com.reservation.user.users.dto.UserPasswordDTO;
 import com.reservation.user.users.service.UserService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -26,4 +29,45 @@ public class UserController {
         userService.saveUser(userDto);
         return ResponseEntity.ok("User registered successfully");
     }
+
+    // 이메일로 사용자 조회
+    @GetMapping("/user")
+    public ResponseEntity<UserEntityDto> getUser(@RequestParam("email") @Email @NotBlank String email) {
+        UserEntityDto userEmail = userService.findByUserEmail(email);
+        if (userEmail == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userEmail);
+    }
+
+    // 아이디로 사용자 조회
+    @GetMapping("/users")
+    public ResponseEntity<List<UserEntityDto>> getAllUsers() {
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    // 사용자 업데이트
+    @PostMapping("/user/update/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable("id") Long id, @RequestBody @Valid UserDto userDto) {
+        userService.updateUser(id, userDto);
+        return ResponseEntity.ok("User updated successfully");
+    }
+
+    // 사용자 활성화 상태 업데이트
+    @PostMapping("/user/active/{id}")
+    public ResponseEntity<?> activeUser(@PathVariable("id") Long id, @RequestParam("active") boolean active) {
+        userService.activeUser(id,active);
+        return ResponseEntity.ok("User active status updated successfully");
+    }
+
+    // 사용자 비밀번호 재설정
+    @PostMapping("/user/password-reset/{id}")
+    public ResponseEntity<?> resetPassword(@PathVariable("id") Long id, @RequestBody @Valid UserPasswordDTO passwordDTO) {
+        userService.resetPassword(id, passwordDTO);
+        return ResponseEntity.ok("User password reset successfully");
+    }
+
+
+
+
 }
